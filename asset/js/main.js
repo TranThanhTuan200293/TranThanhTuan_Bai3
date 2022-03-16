a01d61f4e70c7996ff9dfa2758c7fd587998ad7f
TranThanhTuan200293 <tuantran0416@gmail.com>
2022-03-16 20:40:07 +0700

    16/03

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     // tìm tất cả li có sub-item và thêm vào 
     $('.sub-item').parent('li').addClass('has-child');
 });
-//chưa đc
+// slideshow
 var i=0;
 var images=[];
 var time = 10000;
@@ -29,4 +29,86 @@ $(document).ready(function(){
     $('#toggle').click(function(){
         $('.bosuutap').slideToggle();
     });
+})
+// phần sản phẩm mới
+let sanphammoi_slider = tns({
+    container : ".sanphammoi-my-slider", // đưa class my-slide vào trong container của thư viện trên
+    "slideBy" : 1, // mỗi lần chuyển là 1 slide
+    "speed"   : 600,
+    "nav"     : false,
+    controlsContainer : "#sanphammoi-controls",
+    prevButton : ".sanphammoi-previous",
+    nextButton : ".sanphammoi-next",
+    responsive: {
+        1024: {
+            items : 5,
+            gutter :20
+        },
+        768: {
+            items : 3,
+            gutter :20
+        },
+        480: {
+            items : 1,
+            gutter :20
+        },
+    }
+})
+// phần sản phẩm bán chạy
+let banchay_slider = tns({
+    container : ".banchay-my-slider", // đưa class my-slide vào trong container của thư viện trên
+    "slideBy" : 1, // mỗi lần chuyển là 1 slide
+    "speed"   : 600,
+    "nav"     : false,
+    controlsContainer : "#banchay-controls",
+    prevButton : ".banchay-previous",
+    nextButton : ".banchay-next",
+    responsive: {
+        1024: {
+            items : 5,
+            gutter :20
+        },
+        768: {
+            items : 3,
+            gutter :20
+        },
+        480: {
+            items : 1,
+            gutter :20
+        },
+    }
+})
+///
+let container_sphot = tns({
+    container : ".sanphamhot-slide", // đưa class my-slide vào trong container của thư viện trên
+    "slideBy" : 1, // mỗi lần chuyển là 1 slide
+    "nav":false,
+    "speed"   : 600,
+    
+    controlsContainer : "#sanphamhot-controls",
+    prevButton : ".sanphamhot-previous",
+    nextButton : ".sanphamhot-next",
+    responsive: {
+        1024: {
+            items : 1,
+            gutter:00
+        }
+    }
+    
+})
+let sanphamhot_content = tns({
+    container : ".sanphamhot-slide1", // đưa class my-slide vào trong container của thư viện trên
+    "slideBy" : 1, // mỗi lần chuyển là 1 slide
+    "nav":false,
+    "speed"   : 600,
+    
+    controlsContainer : "#sanphamhot-controls",
+    prevButton : ".sanphamhot-previous",
+    nextButton : ".sanphamhot-next",
+    responsive: {
+        1024: {
+            items : 1
+        }
+    }
+    
 })
